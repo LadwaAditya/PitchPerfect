8dc90ed7e28c13ef03c1e7f220a082cef8da111a
Aditya Ladwa <adityaladwa@adityas-macbook-pro.local>
2018-08-18 12:36:46 +0530

    Feat: Added Button and clicks

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Clicked on button")
+    }
 }
 
